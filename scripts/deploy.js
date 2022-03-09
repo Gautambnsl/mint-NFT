2739840005aaecf145210d96a1264a604c69173b
Gautam Bansal <bansalgautam005@gmail.com>
2022-03-10 03:19:09 +0530

    wooooooooooshhhhhh

---
+++ scripts/deploy.js
@@ -0,0 +1,15 @@
+async function main() {
+    const MyNFT = await ethers.getContractFactory("MyNFT");
+  
+    // Start deployment, returning a promise that resolves to a contract object
+    const myNFT = await MyNFT.deploy();
+    console.log("Contract deployed to address:", myNFT.address);
+  }
+  
+  main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+      console.error(error);
+      process.exit(1);
+    });
+//0xC9747D38F304d4bFeF0e24ffCA065764B4a1e0cd
